15df850a1467b37cd72a2a1322e6f146a7a1e418
z17cx <mail@z17.cx>
2022-09-20 08:17:07 +0000

    2022-09-20 08:17:07

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ GIT_TOKEN="${4}"
 GPG_URL="${5}"
 GPG_NAME="${6}"
 GPG_CMD="${7}"
-USER_AGENT="Mozilla/5.0 (X11; Linux x86_64; rv:104.0) Gecko/20100101 Firefox/104.0"
+USER_AGENT="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36"
 
 # Apps.
 date="$( command -v date )"
